971e3467f039ddfc87b540ead81723b86eb81404
ErikDanielCabral <theriK@usp.br>
2024-11-27 20:05:40 -0300

    Update blink.py
    Comentando o código

--- blink.py
+++ blink.py
@@ -1,12 +1,12 @@
-from gpiozero import LED
-from time import sleep
+from gpiozero import LED 	# Importa a classe LED da biblioteca gpiozero, que facilita o controle de GPIOs na Raspberry Pi.
+from time import sleep 		# Importa a função sleep, usada para criar pausas entre as ações.
 
-PinLed=LED(25)
+PinLed=LED(25)			# Cria um objeto LED associado ao pino GPIO 25 da Raspberry Pi. Este pino deve estar conectado ao LED.
 
-while True:
-	PinLed.on()
-	print("LED aceso")
-	sleep(1)
-	PinLed.off()
-	print("LED apagado")
-	sleep(1)
+while True:			# Inicia um loop infinito que será executado continuamente.
+	PinLed.on()		# Liga o LED, enviando um sinal HIGH (3,3V) ao pino GPIO 25.
+	print("LED aceso")	# Imprime no terminal que o LED está aceso.
+	sleep(1)		# Aguarda por 1 segundo antes de continuar.
+	PinLed.off()		# Desliga o LED, enviando um sinal LOW (0V) ao pino GPIO 25.
+	print("LED apagado")	# Imprime no terminal que o LED está apagado.
+	sleep(1)		# Aguarda por 1 segundo antes de reiniciar o loop.
